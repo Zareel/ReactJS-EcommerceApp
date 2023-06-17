9dd21a94405a3961e99954cac7094b89338f675d
Zareel Kalam <zareelbwd@gmail.com>
2023-06-17 11:18:31 +0530

    Login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // validation
+  const [requiredField, setRequiredField] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setRequiredField("All fields are required");
+    } else {
+      setRequiredField("");
+    }
+    if (email && password) {
+      console.log(email, password);
+      setEmail("");
+      setPassword("");
+    }
+  };
   return (
     <div className="w-full h-full">
       <div className="w-full auto flex items-center justify-center">
@@ -11,20 +39,30 @@ const Login = () => {
             </h1>
           </div>
           <input
+            onChange={handleEmail}
             type="email"
+            value={email}
             placeholder="email"
             className="border-none outline-none my-2 drop-shadow-md w-full px-2 py-2 rounded-sm"
           />
           <br />
 
           <input
+            onChange={handlePassword}
             type="password"
+            value={password}
             placeholder="Password"
             className="border-none outline-none my-2 drop-shadow-md w-full px-2 py-2 rounded-sm"
           />
+          {requiredField && (
+            <p className="text-red-600 text-xs font-semibold">
+              {requiredField}
+            </p>
+          )}
           <br />
           <div className="w-full flex justify-center mt-4">
             <button
+              onClick={handleLogin}
               type="submit"
               className="bg-gradient-to-r from-violet-300 to-fuchsia-400 text-lg py-1 px-6 rounded-sm flex"
             >
